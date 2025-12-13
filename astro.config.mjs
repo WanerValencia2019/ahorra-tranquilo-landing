902753cbfeee7fe0af4d75a5d9e5dadf39653c90
wanervalencia <valenciawaner@gmail.com>
2025-12-12 22:40:51 -0500

    feat(analytics): remove Analytics component from Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
