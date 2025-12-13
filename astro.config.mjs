7f7711149be3c1b9fe4aeec0b67b0c99e5f0678c
wanervalencia <valenciawaner@gmail.com>
2025-12-12 22:44:08 -0500

    feat: integrate Vercel Analytics into Layout component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: 'static',
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
